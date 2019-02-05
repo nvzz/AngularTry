cc5d8e1c68ca4f35a7b70ed207b7641501b6e295
nvzz <n.vazquez05bm@gmail.com>
2019-02-05 16:52:19 -0600

    New project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,34 +12,6 @@ export class AppComponent implements OnInit{
   
   constructor(){ }
 
-  listNames: INames[] = [
-    {
-      "name": "Alejandro",
-      "age": "23"
-    },
-    {
-      "name": "Norma",
-      "age": "22"
-    }
-  ]
-
-  addData(){
-    const newName = document.getElementById('name') as HTMLInputElement;
-    const newAge = document.getElementById('age') as HTMLInputElement;
-    
-    const send = {
-       "name": newName.value,
-       "age": newAge.value
-     }
-
-     this.listNames.push(send);
-
-     newName.value = '';
-     newAge.value = '';
-
-     alert('New entry added');
-
-  }
 
   ngOnInit() : void { }
   
